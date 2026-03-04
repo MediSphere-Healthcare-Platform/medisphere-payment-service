86ce4ddd4f253b1e9d946c7864be643ce61b7195
AroshXR <aroshanasandeep454@gmail.com>
2026-03-04 16:13:28 +0530

    initial commit

---
+++ src/main/java/com/medisphere/payment/MedispherePaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medisphere.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedispherePaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedispherePaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medisphere/payment/MedispherePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisphere.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedispherePaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
