144f4d5c6f5f041ae3226c42f170fbcab262a7bc
AroshXR <aroshanasandeep454@gmail.com>
2026-04-05 22:07:40 +0530

    application.properties

--- src/main/java/com/medisphere/payment/MedispherePaymentServiceApplication.java
+++ src/main/java/com/medisphere/payment/MedispherePaymentServiceApplication.java
@@ -4,9 +4,11 @@ import com.google.gson.Gson;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MedispherePaymentServiceApplication {
 
 	public static void main(String[] args) {
@@ -20,7 +22,28 @@ public class MedispherePaymentServiceApplication {
 
 	@Bean
 	public Gson gson() {
-		return new Gson();
+		return new com.google.gson.GsonBuilder()
+				.registerTypeAdapter(java.time.LocalDate.class,
+						(com.google.gson.JsonSerializer<java.time.LocalDate>) (src, typeOfSrc,
+								context) -> new com.google.gson.JsonPrimitive(
+										src.format(java.time.format.DateTimeFormatter.ISO_LOCAL_DATE)))
+				.registerTypeAdapter(java.time.LocalDate.class,
+						(com.google.gson.JsonDeserializer<java.time.LocalDate>) (json, typeOfT,
+								context) -> java.time.LocalDate.parse(json.getAsString(),
+										java.time.format.DateTimeFormatter.ISO_LOCAL_DATE))
+				.registerTypeAdapter(java.time.LocalTime.class,
+						(com.google.gson.JsonSerializer<java.time.LocalTime>) (src, typeOfSrc,
+								context) -> new com.google.gson.JsonPrimitive(
+										src.format(java.time.format.DateTimeFormatter.ISO_LOCAL_TIME)))
+				.registerTypeAdapter(java.time.LocalTime.class,
+						(com.google.gson.JsonDeserializer<java.time.LocalTime>) (json, typeOfT,
+								context) -> java.time.LocalTime.parse(json.getAsString(),
+										java.time.format.DateTimeFormatter.ISO_LOCAL_TIME))
+				.registerTypeAdapter(java.util.Optional.class,
+						(com.google.gson.JsonSerializer<java.util.Optional<?>>) (src, typeOfSrc,
+								context) -> src.isPresent() ? context.serialize(src.get())
+										: com.google.gson.JsonNull.INSTANCE)
+				.create();
 	}
 
 }
